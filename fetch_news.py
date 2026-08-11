f15bb4ad693a1d520c53ef26c22423f9acf3e2f3
qiuai73-deb <qiuai73@gmail.com>
2026-08-11 15:48:43 +0800

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -572,7 +572,7 @@ def main():
         f.write(md_content)
 
     # 4. 📢 发送钉钉推送
-    send_dingtalk_msg(important_news, interest_news)
+    send_feishu_msg(important_news, interest_news)
 
     # =============================
     # 保存已经推送过的新闻
